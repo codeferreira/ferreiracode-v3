946d44766d10a77672370c47169ef84152646744
José Carlos Ferreira Júnior <dev.josejunior@gmail.com>
2022-10-18 01:51:12 -0300

    chore: getPosts api handler kick off

---
+++ src/pages/api/getPosts.ts
@@ -0,0 +1,8 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  response.status(200)
+}
